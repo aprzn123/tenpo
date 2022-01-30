6cbdf51f35101369cc87b839e65566fca67fffd1
aprzn <aprzn@aprznpc.hsd1.mi.comcast.net>
2022-01-30 00:12:39 -0500

    connections all connected up

--- timer.py
+++ timer.py
@@ -3,11 +3,15 @@ import socket
 import threading
 import logging
 import sys
+import json
+from playsound import playsound
 
 PORT = 2799
 IP = "127.0.0.1"
 ADDR = (IP, PORT)
-HEADER_LEN = 64
+HEAD_LEN = 32
+FORMAT = "utf-8"
+DISCONNECT_MESSAGE = {"dcon": True}
 
 class Server:
     def __init__(self):
@@ -18,18 +22,43 @@ class Server:
         logging.info(f"New connection from {addr}")
         connected = True
         while connected:
-            msg_len = conn.recv(HEADER_LEN)
+            head = conn.recv(HEAD_LEN).decode(FORMAT)
+            if not head:
+                break;
+            msg_len = int(head)
+            msg = json.loads(conn.recv(msg_len).decode(FORMAT))
+            logging.info(f"message recieved: {msg}")
+            if msg == DISCONNECT_MESSAGE:
+                connected = False
+            threading.Thread(target=lambda: playsound("notif.mp3")).start()
+
+        logging.info(f"Disconnecting from {addr}")
+        conn.close()
 
     def start(self):
         self.server.listen()
         logging.info(f"Listening on port {PORT}")
         while True:
-            con, addr = self.server.accept()
+            conn, addr = self.server.accept()
             thread = threading.Thread(target=self.handle_client, args=(conn, addr))
             thread.start()
 
-def run_client():
-    pass
+class Client:
+    def __enter__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(ADDR)
+        return self
+
+    def __exit__(self, etype, evalue, etrace):
+        self.send(DISCONNECT_MESSAGE)
+
+    def send(self, msg):
+        sendable = json.dumps(msg).encode(FORMAT)
+        msg_len = str(len(sendable)).encode(FORMAT)
+        msg_len += b' ' * (HEAD_LEN - len(msg_len))
+        self.client.send(msg_len)
+        self.client.send(sendable)
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -43,4 +72,5 @@ if __name__ == "__main__":
         server = Server()
         server.start()
     else:
-        pass
+        with Client() as client:
+            pass
